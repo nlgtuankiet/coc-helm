e59a6f2b7b01d721275f944667f376090b5af7fb
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2022-03-08 00:24:26 +0700

    add monitor

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,9 @@
 
 helm repo add jetstack https://charts.jetstack.io
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
 helm repo update
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.crds.yaml
+helm show crds prometheus-community/kube-prometheus-stack | kubectl replace --validate=false --server-side=true -f -
